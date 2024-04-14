d5c51fbf8d955aa74bf5d40d8ac19d07e542d1d2
Androk12 <atico-7@hotmail.com>
2024-04-14 17:10:01 -0500

    Se agregan la estructura general y se corrige error de vista

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import config from './config'
 import router from './routes/index.routes'
 const app = express()
 
-app.set('views', path.dirname(__dirname, 'views'))
-app.set('public', path.dirname(__dirname, 'public'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('public', path.join(__dirname, 'public'))
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
