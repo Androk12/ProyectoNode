b59b2e956452bdb952badf6257ff088b7d735b2f
Androk12 <atico-7@hotmail.com>
2024-04-17 14:15:13 -0500

    Se organizan puertos y queda estructura funcionando

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,13 @@
 import express  from "express"
 import indexController from "../controllers/index.controller"
 
+
 const router = express.Router()
 
 router.get('/', indexController.index)
 
 router.get('/login', indexController.login)
+
+router.get('/registro', indexController.registro)
 
 export default router
