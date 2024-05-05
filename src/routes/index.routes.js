7f959c1e4ecb10d6626df3f22b6ec781bce6b2eb
Androk12 <atico-7@hotmail.com>
2024-05-05 18:41:10 -0500

    Version junta presenta error

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -16,7 +16,6 @@ router.get('/registro', indexController.registro)
 
 router.get('/listarc', indexController.listarPersonas)
 
-<<<<<<< HEAD
 router.get('/productos', indexController.productos)
 
 router.get('/productosmujer', indexController.productos2)
@@ -26,8 +25,6 @@ router.get('/productoshom', indexController.productos3)
 router.get('/productosnino', indexController.productos4)
 
 router.get('/carrito', indexController.carrito)
-=======
 router.post('/guardarPersonas', indexController.guardarPersonas)
->>>>>>> 65a3a2111b5832747c41b16133e9f36283a3aa8f
 
 export default router
